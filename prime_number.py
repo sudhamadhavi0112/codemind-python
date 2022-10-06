9241964fd13f6b6f9af181b3d3032ed5c7534a1c
sudhamadhavi0112 <114071156+sudhamadhavi0112@users.noreply.github.com>
2022-10-06 22:10:21 +0530

    N = int(input()) cnt = 0 for i in range(1, N + 1):   if N%i == 0:      cnt += 1 if cnt == 2:      print("prime") else:      print("not a prime")

--- prime_number.py
+++ prime_number.py
@@ -1,9 +1,10 @@
-N = int(input ())
+N = int(input())
 cnt = 0
-for i in range(1, N +1):
+for i in range(1, N + 1):
   if N%i == 0:
-    cnt += 1
+     cnt += 1
 if cnt == 2:
-   print("prime")
+     print("prime")
 else:
-   print("not a prime")
+     print("not a prime")
+     
