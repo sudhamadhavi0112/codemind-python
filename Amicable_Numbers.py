5293c4282a262d96116f45523a3f00c9f1b9e9f4
sudhamadhavi0112 <114071156+sudhamadhavi0112@users.noreply.github.com>
2022-10-10 21:37:05 +0530

    a = int(input()) b = int(input())
    pfs_a = pfs_b = 0
    
    for i in range(1,a):
       if a % i == 0:
         pfs_a += i
       
    for i in range(1,b):
        if b % i == 0:
         pfs_b += i
        
    if pfs_a == b and pfs_b == a:
        print('Amicable')
    else:
        print('Not Amicable')

---
+++ Amicable_Numbers.py
@@ -0,0 +1,17 @@
+a = int(input())
+b = int(input())
+
+pfs_a = pfs_b = 0
+
+for i in range(1,a):
+   if a % i == 0:
+     pfs_a += i
+   
+for i in range(1,b):
+    if b % i == 0:
+     pfs_b += i
+    
+if pfs_a == b and pfs_b == a:
+    print('Amicable')
+else:
+    print('Not Amicable')
